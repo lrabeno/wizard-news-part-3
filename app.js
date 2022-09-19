5337669bc629da5261c6e4b0fc6f7677dd70bccc
Louis Rabeno <lrabeno@gmail.com>
2022-09-19 18:54:05 -0400

    added router

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
-const client = require("./db");
-const postList = require("./views/postList");
-const postDetails = require("./views/postDetails");
-
+const routes = require("./routes/posts");
 const app = express();
 
+app.use("/posts", routes);
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 
-const baseQuery = "SELECT posts.*, users.name, counting.upvotes FROM posts INNER JOIN users ON users.id = posts.userId LEFT JOIN (SELECT postId, COUNT(*) as upvotes FROM upvotes GROUP BY postId) AS counting ON posts.id = counting.postId\n";
-
-app.get("/", async (req, res, next) => {
-  try {
-    const data = await client.query(baseQuery);
-    res.send(postList(data.rows));
-  } catch (error) { next(error) }
-});
-
-app.get("/posts/:id", async (req, res, next) => {
-  try {
-    const data = await client.query(baseQuery + "WHERE posts.id = $1", [req.params.id]);
-    const post = data.rows[0];
-    res.send(postDetails(post));
-  } catch (error) { next(error) }
+app.get("/", (req, res) => {
+  res.redirect("/posts");
 });
 
 const PORT = 1337;
---
+++ routes/posts.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+// could use one line instead: const router = require('express').Router();
+const client = require("../db");
+const postList = require("../views/postList");
+const postDetails = require("../views/postDetails");
+const addPost = require("../views/postList");
+
+const baseQuery =
+  "SELECT posts.*, users.name, counting.upvotes FROM posts INNER JOIN users ON users.id = posts.userId LEFT JOIN (SELECT postId, COUNT(*) as upvotes FROM upvotes GROUP BY postId) AS counting ON posts.id = counting.postId\n";
+
+router.get("/", async (req, res, next) => {
+  try {
+    const data = await client.query(baseQuery);
+    res.send(postList(data.rows));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/add", (req, res) => {
+  res.send(addPost());
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const data = await client.query(baseQuery + "WHERE posts.id = $1", [
+      req.params.id,
+    ]);
+    const post = data.rows[0];
+    res.send(postDetails(post));
+  } catch (error) {
+    next(error);
+  }
+});
+
+// router.get("/", async (req, res, next) => {
+//   try {
+//     const data = await client.query(/*Omitted for brevity*/);
+//     res.send(postList(data.rows));
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const data = await client.query(/*Omitted for brevity*/);
+//     const post = data.rows[0];
+//     res.send(postDetails(post));
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+module.exports = router;
